ab0202020d5dd81fd928b04a8f2d45cca7ac0137
Robert Eves <roberteves92@gmail.com>
2018-12-19 19:42:25 +0000

    correct app name

--- HertfordshireMercury/HertfordshireMercury.Android/MainActivity.cs
+++ HertfordshireMercury/HertfordshireMercury.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using Android.Gms.Ads;
 
 namespace BSIndie.Droid
 {
-    [Activity(Label = "Hertfordshire Mercury", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Stortford Independent", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
